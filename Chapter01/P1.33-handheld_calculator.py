c507bc10aebeba1546c6eaf88107e9725a02c4a2
rshinoha <r.shinohara@zoho.com>
2020-04-09 20:18:10 -0500

    finished all helper functions; debugging one edge case

--- Chapter01/P1.33-handheld_calculator.py
+++ Chapter01/P1.33-handheld_calculator.py
@@ -14,6 +14,10 @@ clear = 'c'
 off = 'o'
 # Symbol for error
 error = 'e'
+# All operators
+operators = ['+', '-', '*', '/', '=']
+# All numbers
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def get_num(str_input):
 	"""
@@ -30,12 +34,81 @@ def get_num(str_input):
 			numeric_symbols[0] = ''
 		if str_input[i] == '.':
 			numeric_symbols[1] = ''
+	if len(num_str) == 0:
+		return 0
 	if '.' in num_str:
 		return float(num_str)
 	else:
 		return int(num_str)
 
-tempStr = input('Enter a number: ')
-tempNum = get_num(tempStr)
-print(tempNum)
-print(type(tempNum))
+def get_operator(str_input):
+	"""
+	Given a string, str_input, returns the last valid operator in the
+	string or an empty string if no valid operator is found
+	"""
+	found_operator = False
+	operator = ''
+	i = len(str_input) - 1
+	while i >= 0 and not found_operator:
+		if str_input[i] in operators:
+			found_operator = True
+			operator = str_input[i]
+		i -= 1
+	return operator
+
+def calculate(operator, new_num, cur_num = 0):
+	"""
+	Given two numbers, cur_num and new_num, and an operator
+	character, operator, calculates the solution. The default value
+	for cur_num is 0.
+	"""
+	solution = 0
+	if operator == '+':
+		solution = cur_num + new_num
+	elif operator == '-':
+		solution = cur_num - new_num
+	elif operator == '*' :
+		solution = cur_num * new_num
+	elif operator == '/':
+		solution = cur_num / new_num
+	elif operator == '=':
+		solution = cur_num
+	return solution
+
+def main():
+	print("Mock Handheld Calculator")
+	print("========================")
+	clear_symbol = 'c'
+	off_symbol = 'o'
+	off = False
+	cur_num = 0
+	new_num = 0
+	operator = ''
+	while not off:
+		str_input = input().lower()
+		if len(str_input) == 0:
+			pass
+		elif str_input[0] == clear_symbol:
+			cur_num = 0
+		elif str_input[0] == off_symbol:
+			off = True
+		elif sum([int(char in numbers) for char in str_input]) > 0:
+			new_num = get_num(str_input)
+			if operator == '':
+				cur_num = new_num
+				new_num = 0
+			elif operator == '=':
+				operator = ''
+			else:
+				cur_num = calculate(operator, new_num, cur_num)
+				operator = ''
+		else:
+			operator = get_operator(str_input)
+			while operator == '':
+				str_input = input()
+				operator = get_operator(str_input)
+		if not off:
+			print(cur_num)
+
+if __name__ == "__main__":
+	main()
